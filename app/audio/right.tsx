ee7f2a59b490a61b2296d62cd42c6e97a073f47e
Pullur Bhavana <141913627+bhavpullur@users.noreply.github.com>
2024-04-01 21:24:19 +0530

    commit

--- app/audio/right.tsx
+++ app/audio/right.tsx
@@ -1,52 +1,51 @@
-import React from "react";
-import Image from "next/image";
+"use client"
+import React from 'react'
+import Image from 'next/image';
+import { useState, useRef } from 'react';
+
+
 const Right = () => {
-  return (
-    <div className="flex  flex-col  items-center mx-9 my-16 gap-4">
-      <div className="flex w-full justify-center items-center">
-        {/*image  */}
-        <Image
-          src={"/magic.jpg"}
-          height={300}
-          width={300}
-          alt="magic"
-          className="rounded-md   border border-white "
-        ></Image>
-      </div>
-      {/* text */}
-      <div className="flex jost text-white text-[20px] justify-center">
-        THE MAGIC STAR
-      </div>
-      {/* play button */}
-      <div className="flex flex-row justify-center items-center gap-1">
-        {/* time */}
-        <div className="flex text-white text-[10px] ">12:26</div>
-        {/* play line */}
-        <div className="audio-player">
-        <audio controls >
-           <source src='/audiobook_ex.mp3' />
-        </audio>
+    const [isPlaying, setIsPlaying] = useState(false);
+    const audioRef = useRef<HTMLAudioElement>(null);
+    const togglePlay = () => {
+        if (audioRef.current) {
+          if (isPlaying) {
+            audioRef.current.pause();
+          } else {
+            audioRef.current.play();
+          }
+          setIsPlaying(!isPlaying);
+        }
+    };
+
+    return (
+        <div className="flex  flex-col  items-center mx-9 my-16">
+        <div className="flex w-full justify-center items-center">
+            {/*image  */}
+            <Image
+            src={"/images/audiobook_example.jpg"}
+            height={300}
+            width={300}
+            alt="magic"
+            className="rounded-md   border border-white "
+            ></Image>
         </div>
-        {/* <div className="flex">
-          <svg
-            width="234"
-            height="7"
-            viewBox="0 0 234 7"
-            fill="none"
-            xmlns="http://www.w3.org/2000/svg"
-          >
-            <rect width="234" height="7" rx="3.5" fill="#303030" />
-            <rect width="36" height="7" fill="#E9E9E9" />
-          </svg>
-        </div> */}
-      </div>
-      {/* like pause save */}
-      <div>
-        {/*like */}
-        <div></div>
-      </div>
-    </div>
-  );
+        {/* text */}
+        <div className="flex jost text-white text-[20px] justify-center">
+            THE MAGIC STAR
+        </div>
+        {/* Audio Player */}
+        <div>
+            <button onClick={togglePlay} className="bg-black text-white rounded-full p-4 focus:outline-none">
+                {isPlaying ? 
+                <Image src={"/icons/play.png"} width={34} height={34} alt='play'></Image>
+                : 
+                <Image src={"/icons/pause.png"} width={34} height={34} alt='pause'></Image>}
+            </button>
+            <audio ref={audioRef} src="/audio/audiobook_example.mp3" className="mt-4 w-full" />
+        </div>
+        </div>
+    );
 };
 
 export default Right;
